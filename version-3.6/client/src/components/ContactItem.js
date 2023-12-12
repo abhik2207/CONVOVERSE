80eb92b30733c3e3bbd9bd4e5efce036c4c10484
Abhik <abhikgupta01@gmail.com>
2023-12-12 19:07:03 +0530

    Twelth commit

--- version-3.6/client/src/components/ContactItem.js
+++ version-3.6/client/src/components/ContactItem.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './ContactItem.css';
+import { FaCheckCircle } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 
 const ContactItem = (props) => {
     const { userInfo, setChatPlaceHolder, setSelectedChat } = props;
     const [userDisplayData, setUserDisplayData] = useState({ userDisplayPic: '', userDisplayName: '' });
     const [lastMessage, setLastMessage] = useState('');
-    const [lastMessageSign, setLastMessageSign] = useState('');
+    const [lastMessageSign, setLastMessageSign] = useState();
 
     function fetchContactData() {
         if (localStorage.getItem('convoverseUserLoginId') === userInfo.channelUsers[0]._id) {
@@ -17,17 +19,14 @@ const ContactItem = (props) => {
     }
 
     function fetchLastMessage() {
-        if(userInfo.messages.length === 0) {
-            setLastMessage('');
-        }
-        else {
+        if (userInfo.messages.length > 0) {
             const length = userInfo.messages.length;
             const lastMsg = userInfo.messages[length - 1];
-            if(lastMsg.senderID === localStorage.getItem('convoverseUserLoginId')){
-                setLastMessageSign('✔');
+            if (lastMsg.senderID === localStorage.getItem('convoverseUserLoginId')) {
+                setLastMessageSign(0);
             }
             else {
-                setLastMessageSign('🟢');
+                setLastMessageSign(1);
             }
             const lastMsgText = lastMsg.message.slice(0, 40);
             setLastMessage(lastMsgText);
@@ -39,9 +38,10 @@ const ContactItem = (props) => {
         fetchLastMessage();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         fetchLastMessage();
+        console.log('hello');
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [userInfo.messages]);
 
@@ -60,7 +60,7 @@ const ContactItem = (props) => {
                 </div>
             </div>
             <div className="rightSection">
-                <p>{lastMessageSign}</p>
+                {lastMessageSign === 0 ? <p className='sentSign'><FaCheckCircle /></p> : <p className='recievedSign'><FaEnvelope /></p>}
             </div>
         </div>
     )
